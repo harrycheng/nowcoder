a29ab82c81ffc0c6083c008d540bf6eff3df305a
chenghao <chenghao@bj-fanuc.com.cn>
2021-07-12 22:33:37 +0800

    TreeNodeMirror

--- src/main/java/com/hc/nowcoder/TreeNodeMirror.java
+++ src/main/java/com/hc/nowcoder/TreeNodeMirror.java
@@ -1,8 +1,48 @@
 package com.hc.nowcoder;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class TreeNodeMirror {
-    public TreeNode Mirror(TreeNode pRoot) {
+    public TreeNode Mirror_recursion(TreeNode pRoot) {
         // write code here
-        return null;
+        if (pRoot == null) {
+            return null;
+        }
+
+        TreeNode temp = pRoot.left;
+        pRoot.left = pRoot.right;
+        pRoot.right = temp;
+
+        Mirror_recursion(pRoot.left);
+        Mirror_recursion(pRoot.right);
+        return pRoot;
+    }
+
+    public TreeNode Mirror_notrecursion(TreeNode pRoot) {
+        // write code here
+        if (pRoot == null) {
+            return null;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(pRoot);
+        while ( !queue.isEmpty()){
+
+           TreeNode cur =  queue.poll();
+            TreeNode temp =  cur.left;
+            cur.left = cur.right;
+            cur.right = temp;
+
+            if(cur.left!=null){
+                queue.add(cur.left);
+            }
+
+
+            if(cur.right!=null){
+                queue.add(cur.right);
+            }
+        }
+        return pRoot;
     }
 }
